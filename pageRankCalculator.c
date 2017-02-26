4b41d597b4e39d3139f36984f4cd05586d6613d8
IamThilina <333thilina@gmail.com>
2017-02-26 12:06:13 +0530

    Implemented Page Rank Algorithm Without Termination Condition

--- pageRankCalculator.c
+++ pageRankCalculator.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+#include <time.h>
 
 #define MAXIMUM_NUMBER 10
+#define MAX_LINKS_FROM_PAGES 5
 
 void print2DArray(double *array, int size){
 
@@ -28,29 +29,54 @@ void print1DArray(double *array, int size){
 
 void initializeProbabilityTransitionMatrix(double *mat, int size){
 
+    int nnz; // number of non-zero values in a column
+    int count; // number of non-zero element already added
+    int randomRow;
+
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i) {
+        nnz = ((int)rand()%MAX_LINKS_FROM_PAGES)+5; // nnz take a value 5 to 20
+        //printf("%f\n",(double)1/nnz);
+        count = 0;
+        while (count < nnz){
+            count++;
+            randomRow = ((int)rand()%size);
+            //printf("%d ",randomRow);
+            *((mat+randomRow*size)+i) = (double)1/nnz;
+        }
+        //printf("\n");
+    }
 }
 
 void initializePageRankVector(double *vect, int size){
-
+    //memset(vect, 1, size*sizeof(double));
+    for (int i = 0; i < size; ++i) {
+        *(vect+i) = (double) 1/size;
+    }
 }
 
 int main() {
 
-    const int N = 1000;
+    const int N = 10;
     int i,j,iteration=0;
     double *probabilityTransitionMat;
     double temp, absoluteError,relativeError = 1;
     double *pageRankVector, *pageRankVectorCopy;
 
-    printf("\n**********************************  Matrix  *********************************\n");
-    probabilityTransitionMat = (double*) malloc(N*N*sizeof(double));
+    //printf("\n**********************************  Matrix  *********************************\n\n");
+    probabilityTransitionMat = (double*) calloc(N*N, sizeof(double)); // allocate memory and initialize to zero
     pageRankVector = (double*) malloc(N*sizeof(double));
     pageRankVectorCopy = (double*) malloc(N*sizeof(double));
 
     initializeProbabilityTransitionMatrix(probabilityTransitionMat, N);
     initializePageRankVector(pageRankVectorCopy, N);
 
-    while (iteration < 10){
+    //print2DArray(probabilityTransitionMat, N);
+    //printf("\n\n");
+    //print1DArray(pageRankVectorCopy, N);
+
+
+    while (iteration < 2){
         iteration++;
         printf("\n*****************************  Iteration No : %d  ***************************\n",iteration);
         for (i = 0; i < N; ++i) {
@@ -63,7 +89,7 @@ int main() {
         memcpy(pageRankVectorCopy,pageRankVector, N);
         printf("\nPage Ranks\n");
         print1DArray(pageRankVector, N);
-        printf("\n*****************************************************************************\n");
+        printf("\n\n*****************************************************************************\n");
     }
 
     /*releasing allocated memory blocks*/
